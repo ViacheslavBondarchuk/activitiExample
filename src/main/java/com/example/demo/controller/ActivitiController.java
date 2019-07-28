f1bf5103b140cd382c7ca5057fbe7f9f4e6d7b8d
Viacheslav <slava.777.bondarchuk@outlook.com>
2019-07-28 23:49:04 +0300

    init

--- src/main/java/com/example/demo/controller/ActivitiController.java
+++ src/main/java/com/example/demo/controller/ActivitiController.java
@@ -4,12 +4,12 @@ import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping
 public class ActivitiController {
     @Autowired
     private RepositoryService repositoryService;
@@ -19,9 +19,8 @@ public class ActivitiController {
     private TaskService taskService;
 
     @GetMapping
-    public void startProcess(){
-        System.out.println("Process has been started");
-
+    public String startProcess(){
+       return "Process has been started";
     }
 
 }
